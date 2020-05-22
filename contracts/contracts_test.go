f52a75e639bf7d20028189bb2db41680788f2ecd
Benjamin Van Meter <54324957+benjaminkvm@users.noreply.github.com>
2020-05-22 15:50:50 -0400

    Add contracts module containing FungibleToken/FlowToken (#13)
    * Add contracts module
    
    * Pull contracts from repo, include test

---
+++ contracts/contracts_test.go
@@ -0,0 +1,22 @@
+package contracts_test
+
+import (
+	"testing"
+
+	"github.com/onflow/flow-ft/contracts"
+	"github.com/onflow/flow-go-sdk"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFungibleTokenContract(t *testing.T) {
+	contract, err := contracts.FungibleTokenContract()
+	assert.NotNil(t, contract)
+	assert.NoError(t, err)
+}
+
+func TestFlowTokenContract(t *testing.T) {
+	contract, err := contracts.FlowTokenContract(flow.Address{0x3})
+	assert.NotNil(t, contract)
+	assert.NoError(t, err)
+	assert.Contains(t, string(contract), "0x03")
+}
---
+++ contracts/go.mod
@@ -0,0 +1,8 @@
+module github.com/onflow/flow-ft/contracts
+
+go 1.14
+
+require (
+	github.com/onflow/flow-go-sdk v0.1.1
+	github.com/stretchr/testify v1.5.1
+)
