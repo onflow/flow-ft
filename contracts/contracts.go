45af44ee81bceefe1d4af458d848aa21608c4c63
Peter Siemens <peterjsiemens@gmail.com>
2020-05-24 16:41:32 -0700

    contracts: Remove dependency on flow-go-sdk (#18)
    * contracts: Remove dependency on flow-go-sdk
    
    * Update contracts.go

--- contracts/contracts.go
+++ contracts/contracts.go
@@ -1,34 +1,34 @@
 package contracts
 
-//go:generate go run github.com/kevinburke/go-bindata/go-bindata -prefix ../src/contracts -o assets.go -pkg contracts -nometadata -nomemcopy ../src/contracts
+//go:generate go run github.com/kevinburke/go-bindata/go-bindata -prefix ../src/contracts -o internal/assets/assets.go -pkg assets -nometadata -nomemcopy ../src/contracts
 
 import (
 	"strings"
 
-	"github.com/onflow/flow-go-sdk"
+	"github.com/onflow/flow-ft/contracts/internal/assets"
 )
 
 const (
-	FungibleTokenContractFilename = "FungibleToken.cdc"
-	FlowTokenContractFilename     = "FlowToken.cdc"
-	defaultFungibleTokenAddress   = "02"
+	fungibleTokenFilename       = "FungibleToken.cdc"
+	flowTokenFilename           = "FlowToken.cdc"
+	defaultFungibleTokenAddress = "02"
 )
 
 // FungibleToken returns the FungibleToken contract interface.
 func FungibleToken() []byte {
-	return MustAsset(FungibleTokenContractFilename)
+	return assets.MustAsset(fungibleTokenFilename)
 }
 
-// FlowToken returns the FlowToken contract. importing the
+// FlowToken returns the FlowToken contract.
 //
 // The returned contract will import the FungibleToken contract from the specified address.
-func FlowToken(fungibleTokenAddress flow.Address) []byte {
-	code := MustAssetString(FlowTokenContractFilename)
+func FlowToken(fungibleTokenAddr string) []byte {
+	code := assets.MustAssetString(flowTokenFilename)
 
 	code = strings.ReplaceAll(
 		code,
 		"0x"+defaultFungibleTokenAddress,
-		"0x"+fungibleTokenAddress.Hex(),
+		"0x"+fungibleTokenAddr,
 	)
 
 	return []byte(code)
