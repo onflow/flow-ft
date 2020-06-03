a2d3214cfbe7dd8154bc3b83a036fea6747c01dc
joshuahannan <joshua.hannan@dapperlabs.com>
2020-06-03 14:53:36 -0500

    using better string templating

--- test/templates.go
+++ test/templates.go
@@ -12,45 +12,45 @@ import (
 // The Vault must have been deployed already.
 func GenerateCreateTokenScript(fungibleAddr, tokenAddr flow.Address, tokenName, storageName string) []byte {
 	template := `
-	  import FungibleToken from 0x%s 
-	  import %s from 0x%s
+	  import FungibleToken from 0x%[1]s 
+	  import %[3]s from 0x%[2]s
 
       transaction {
 
           prepare(acct: AuthAccount) {
-              let vault <- %s.createEmptyVault()
-              acct.save(<-vault, to: /storage/%sVault)
+              let vault <- %[3]s.createEmptyVault()
+              acct.save(<-vault, to: /storage/%[4]sVault)
 
-              acct.link<&%s.Vault{FungibleToken.Receiver}>(/public/%sReceiver, target: /storage/%sVault)
-              acct.link<&%s.Vault{FungibleToken.Balance}>(/public/%sBalance, target: /storage/%sVault)
+              acct.link<&%[3]s.Vault{FungibleToken.Receiver}>(/public/%[4]sReceiver, target: /storage/%[4]sVault)
+              acct.link<&%[3]s.Vault{FungibleToken.Balance}>(/public/%[4]sBalance, target: /storage/%[4]sVault)
           }
       }
     `
-	return []byte(fmt.Sprintf(template, fungibleAddr, tokenName, tokenAddr, tokenName, storageName, tokenName, storageName, storageName, tokenName, storageName, storageName))
+	return []byte(fmt.Sprintf(template, fungibleAddr, tokenAddr, tokenName, storageName))
 }
 
 // GenerateDestroyVaultScript creates a script that withdraws
 // tokens from a vault and destroys the tokens
 func GenerateDestroyVaultScript(fungibleAddr, tokenAddr flow.Address, tokenName, storageName string, withdrawAmount int) []byte {
 	template := `
-		import FungibleToken from 0x%s 
-		import %s from 0x%s
+		import FungibleToken from 0x%[1]s 
+		import %[3]s from 0x%[2]s
 
 		transaction {
 		  prepare(acct: AuthAccount) {
-			let vault <- acct.load<@%s.Vault>(from: /storage/%sVault)
+			let vault <- acct.load<@%[3]s.Vault>(from: /storage/%[4]sVault)
 				?? panic("Couldn't load Vault from storage")
 			
-			let withdrawVault <- vault.withdraw(amount: %d.0)
+			let withdrawVault <- vault.withdraw(amount: %[5]d.0)
 
-			acct.save(<-vault, to: /storage/%sVault) 
+			acct.save(<-vault, to: /storage/%[4]sVault) 
 
 			destroy withdrawVault
 		  }
 		}
 	`
 
-	return []byte(fmt.Sprintf(template, fungibleAddr, tokenName, tokenAddr, tokenName, storageName, withdrawAmount, storageName))
+	return []byte(fmt.Sprintf(template, fungibleAddr, tokenAddr, tokenName, storageName, withdrawAmount))
 }
 
 // GenerateTransferVaultScript creates a script that withdraws an tokens from an account
@@ -84,27 +84,27 @@ func GenerateTransferVaultScript(fungibleAddr, tokenAddr flow.Address, receiverA
 // to mint new tokens and deposit them in a Vault
 func GenerateMintTokensScript(fungibleAddr, tokenAddr flow.Address, receiverAddr flow.Address, tokenName, storageName string, amount float64) []byte {
 	template := `
-		import FungibleToken from 0x%s 
-		import %s from 0x%s
+		import FungibleToken from 0x%[1]s 
+		import %[3]s from 0x%[2]s
 	
 		transaction {
-			let tokenAdmin: &%s.Administrator
-			let tokenReceiver: &%s.Vault{FungibleToken.Receiver}
+			let tokenAdmin: &%[3]s.Administrator
+			let tokenReceiver: &%[3]s.Vault{FungibleToken.Receiver}
 	
 			prepare(signer: AuthAccount) {
 			  self.tokenAdmin = signer
-				.borrow<&%s.Administrator>(from: /storage/%sAdmin) 
+				.borrow<&%[3]s.Administrator>(from: /storage/%[4]sAdmin) 
 				?? panic("Signer is not the token admin")
 	
-			  self.tokenReceiver = getAccount(0x%s)
-				.getCapability(/public/%sReceiver)!
-				.borrow<&%s.Vault{FungibleToken.Receiver}>()
+			  self.tokenReceiver = getAccount(0x%[5]s)
+				.getCapability(/public/%[4]sReceiver)!
+				.borrow<&%[3]s.Vault{FungibleToken.Receiver}>()
 				?? panic("Unable to borrow receiver reference")
 			}
 	
 			execute {
 			  let minter <- self.tokenAdmin.createNewMinter(allowedAmount: 100.0)
-			  let mintedVault <- minter.mintTokens(amount: %f)
+			  let mintedVault <- minter.mintTokens(amount: %[6]f)
 	
 			  self.tokenReceiver.deposit(from: <-mintedVault)
 	
@@ -113,31 +113,31 @@ func GenerateMintTokensScript(fungibleAddr, tokenAddr flow.Address, receiverAddr
 		  }
 	`
 
-	return []byte(fmt.Sprintf(template, fungibleAddr, tokenName, tokenAddr, tokenName, tokenName, tokenName, storageName, receiverAddr, storageName, tokenName, amount))
+	return []byte(fmt.Sprintf(template, fungibleAddr, tokenAddr, tokenName, storageName, receiverAddr, amount))
 }
 
 // GenerateBurnTokensScript creates a script that uses the admin resource
 // to destroy tokens and deposit them in a Vault
 func GenerateBurnTokensScript(fungibleAddr, tokenAddr flow.Address, tokenName, storageName string, amount int) []byte {
 	template := `
-	import FungibleToken from 0x%s 
-	import %s from 0x%s
+	import FungibleToken from 0x%[1]s 
+	import %[3]s from 0x%[2]s
 	
 	transaction {
 	
 		// Vault resource that holds the tokens that are being burned
 		let vault: @FungibleToken.Vault
 	
-		let admin: &%s.Administrator
+		let admin: &%[3]s.Administrator
 	
 		prepare(signer: AuthAccount) {
 	
-			// Withdraw 10 tokens from the admin vault in storage
-			self.vault <- signer.borrow<&%s.Vault>(from: /storage/%sVault)!
-				.withdraw(amount: UFix64(%d))
+			// Withdraw tokens from the admin vault in storage
+			self.vault <- signer.borrow<&%[3]s.Vault>(from: /storage/%[4]sVault)!
+				.withdraw(amount: UFix64(%[5]d))
 	
 			// Create a reference to the admin admin resource in storage
-			self.admin = signer.borrow<&%s.Administrator>(from: /storage/%sAdmin)
+			self.admin = signer.borrow<&%[3]s.Administrator>(from: /storage/%[4]sAdmin)
 				?? panic("Could not borrow a reference to the admin resource")
 		}
 	
@@ -151,7 +151,7 @@ func GenerateBurnTokensScript(fungibleAddr, tokenAddr flow.Address, tokenName, s
 	}
 	`
 
-	return []byte(fmt.Sprintf(template, fungibleAddr, tokenName, tokenAddr, tokenName, tokenName, storageName, amount, tokenName, storageName))
+	return []byte(fmt.Sprintf(template, fungibleAddr, tokenAddr, tokenName, storageName, amount))
 }
 
 // GenerateInspectVaultScript creates a script that retrieves a
@@ -159,21 +159,21 @@ func GenerateBurnTokensScript(fungibleAddr, tokenAddr flow.Address, tokenName, s
 // its balance. If these assertions fail, the script panics.
 func GenerateInspectVaultScript(fungibleAddr, tokenAddr, userAddr flow.Address, tokenName, storageName string, expectedBalance float64) []byte {
 	template := `
-		import FungibleToken from 0x%s 
-		import %s from 0x%s
+		import FungibleToken from 0x%[1]s 
+		import %[3]s from 0x%[2]s
 
 		pub fun main() {
-			let acct = getAccount(0x%s)
-			let vaultRef = acct.getCapability(/public/%sBalance)!.borrow<&%s.Vault{FungibleToken.Balance}>()
+			let acct = getAccount(0x%[5]s)
+			let vaultRef = acct.getCapability(/public/%[4]sBalance)!.borrow<&%[3]s.Vault{FungibleToken.Balance}>()
 				?? panic("Could not borrow Balance reference to the Vault")
 			assert(
-                vaultRef.balance == UFix64(%f),
+                vaultRef.balance == UFix64(%[6]f),
                 message: "incorrect balance!"
             )
 		}
     `
 
-	return []byte(fmt.Sprintf(template, fungibleAddr, tokenName, tokenAddr, userAddr, storageName, tokenName, expectedBalance))
+	return []byte(fmt.Sprintf(template, fungibleAddr, tokenAddr, tokenName, storageName, userAddr, expectedBalance))
 }
 
 // GenerateInspectSupplyScript creates a script that reads
@@ -181,16 +181,16 @@ func GenerateInspectVaultScript(fungibleAddr, tokenAddr, userAddr flow.Address, 
 // and makes assertions about the number
 func GenerateInspectSupplyScript(fungibleAddr, tokenAddr flow.Address, tokenName string, expectedSupply int) []byte {
 	template := `
-		import FungibleToken from 0x%s 
-		import %s from 0x%s
+		import FungibleToken from 0x%[1]s 
+		import %[3]s from 0x%[2]s
 
 		pub fun main() {
 			assert(
-                %s.totalSupply == UFix64(%d),
+                %[3]s.totalSupply == UFix64(%[4]d),
                 message: "incorrect totalSupply!"
             )
 		}
 	`
 
-	return []byte(fmt.Sprintf(template, fungibleAddr, tokenName, tokenAddr, tokenName, expectedSupply))
+	return []byte(fmt.Sprintf(template, fungibleAddr, tokenAddr, tokenName, expectedSupply))
 }
